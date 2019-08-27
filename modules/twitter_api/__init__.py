46d6704ddef98caf56a83f48d1ba245ae6b800d7
estebanpdl <pdlr.estebanfelipe@gmail.com>
2019-08-27 15:51:04 -0500

    Update

--- modules/twitter_api/__init__.py
+++ modules/twitter_api/__init__.py
@@ -36,6 +36,13 @@ def twitter_timeline_columns():
         'user_id_str',
 		'user_url',
         'created_at',
+        'created_at_timestamp',
+        'created_at_year',
+        'created_at_month',
+        'created_at_day',
+		'created_at_hour',
+		'created_at_weekday',
+		'created_at_time_hour',
         'text',
 		'full_text',
 		'is_retweet_status',
@@ -43,6 +50,12 @@ def twitter_timeline_columns():
 		'rt_user_id_str',
 		'rt_status_id_str',
 		'is_quote_status',
+		'quoted_status_id_str',
+		'quoted_status_url',
+		'quoted_user_screen_name',
+		'in_reply_to_status_id_str',
+		'in_reply_to_screen_name',
+		'in_reply_to_user_id_str',
 		'source'
     ]
 
@@ -111,6 +124,212 @@ def retweet_attributes(data):
 	}
 
 
+def quote_attributes(data):
+	'''
+	'''
+	qstatus = 'is_quote_status'
+	link = 'quoted_status_permalink'
+	return {
+		'quoted_status_id_str': [
+			(
+				obj['quoted_status_id_str']
+				if obj[qstatus] == True else None
+			)
+			for obj in data
+		],
+		'quoted_status_url': [
+			(
+				obj[link]['expanded']
+				if obj[qstatus] == True else None
+			)
+			for obj in data
+		],
+		'quoted_user_screen_name': [
+			(
+				obj[link]['expanded'].split('/')[3]
+				if obj[qstatus] == True else None
+			)
+			for obj in data
+		]
+	}
+
+
+def reply_attributes(data):
+	'''
+	'''
+	return {
+		'in_reply_to_status_id_str': [
+			obj['in_reply_to_status_id_str'] for obj in data
+		],
+		'in_reply_to_screen_name': [
+			(
+				obj['in_reply_to_screen_name']
+				if obj['in_reply_to_status_id_str'] != None
+				else None
+			)
+			for obj in data
+		],
+		'in_reply_to_user_id_str': [
+			(
+				obj['in_reply_to_user_id_str']
+				if obj['in_reply_to_status_id_str'] != None
+				else None
+			)
+			for obj in data
+		]
+	}
+
+
+def timestamp_attrs(data, col='created_at', tz='America/Mexico_City',
+	passtz=True):
+	'''
+	'''
+	if passtz:
+		t = pd.to_datetime(
+			data[col],
+			utc=True,
+			infer_datetime_format=True
+		).apply(lambda x: x.tz_convert(tz))
+	else:
+		t = pd.to_datetime(
+			data[col],
+			infer_datetime_format=True
+		)
+	
+	data[f'{col}_timestamp'] = t
+	data[f'{col}'] = t.dt.strftime('%Y-%m-%d')
+	data[f'{col}_year'] = t.dt.year
+	data[f'{col}_month'] = t.dt.month
+	data[f'{col}_day'] = t.dt.day
+	data[f'{col}_hour'] = t.dt.hour
+	data[f'{col}_weekday'] = t.dt.dayofweek
+	data[f'{col}_time_hour'] = t.dt.strftime('%H:%M:%S')
+	
+	return data
+
+
+def build_dataframe(statuses, tz):
+	'''
+	'''
+	_id = [i['id_str'] for i in statuses]
+	tweet_url = [
+		f'https://twitter.com/i/web/status/{i}'
+		for i in _id
+	]
+	user = [i['user']['screen_name'] for i in statuses]
+	user_id_str = [i['user']['id_str'] for i in statuses]
+	user_url = [
+		f'https://twitter.com/{i}' for i in user
+	]
+	created_at = [i['created_at'] for i in statuses]
+	text = [i['full_text'] for i in statuses]
+	full_text = extract_full_text(statuses)
+	isrt = is_retweet_status(statuses)
+	isq = is_quote_status(statuses)
+	source = [get_anchor_text(i['source']) for i in statuses]
+
+	# retweet attributes
+	rt_attrs = retweet_attributes(statuses)
+
+	# quote attributes
+	qt_attrs = quote_attributes(statuses)
+
+	# reply attrs
+	rp_attrs = reply_attributes(statuses)
+
+	# create dataframe
+	df = pd.DataFrame({
+		'id_str': _id,
+		'tweet_url': tweet_url,
+		'user': user,
+		'user_id_str': user_id_str,
+		'user_url': user_url,
+		'created_at': created_at,
+		'text': text,
+		'full_text': full_text,
+		'is_retweet_status': isrt,
+		'rt_user_screen_name': rt_attrs['rt_user_screen_name'],
+		'rt_user_id_str': rt_attrs['rt_user_id_str'],
+		'rt_status_id_str': rt_attrs['rt_status_id_str'],
+		'is_quote_status': isq,
+		'quoted_status_id_str': qt_attrs['quoted_status_id_str'],
+		'quoted_status_url': qt_attrs['quoted_status_url'],
+		'quoted_user_screen_name': qt_attrs['quoted_user_screen_name'],
+		'in_reply_to_status_id_str': rp_attrs['in_reply_to_status_id_str'],
+		'in_reply_to_screen_name': rp_attrs['in_reply_to_screen_name'],
+		'in_reply_to_user_id_str': rp_attrs['in_reply_to_user_id_str'],
+		'source': source
+	})
+
+	# clean text
+	df['text'] = df['text'].apply(clean_text)
+	df['full_text'] = df['full_text'].apply(clean_text)
+
+	# timestamp attributes
+	args = {
+		'data': df
+	}
+	if tz != 'default':
+		args['tz'] = tz
+	
+	df = timestamp_attrs(**args)
+
+	# order columns
+	order_path = '../assets/user_timeline/columns_order.txt'
+	order = [i.rstrip() for i in open(order_path)]
+	df = df[order].copy()
+
+	return df
+
+
+def user_timeline(cxn, **kwargs):
+	'''
+	'''
+	# create time string folder
+	folder = time_string_folder()
+
+	# create csv files -> twitter data
+	path = f'{folder}/data.csv'
+	pd.DataFrame(columns=twitter_timeline_columns()) \
+		.to_csv(path, index=False, encoding='utf-8')
+	
+	# arguments
+	tz = 'default'
+	if 'tz' in kwargs.keys():
+		tz = kwargs['tz']
+		del kwargs['tz']
+	
+	args = arguments(**kwargs)
+	statuses = cxn.statuses.user_timeline(**args)
+
+
+	# temp
+	df = build_dataframe(statuses, tz)
+
+	# append data to csv file
+	df.to_csv(path, index=False, encoding='utf-8', mode='a', \
+		header=False)
+
+
+	# get oldest
+	oldest = min([i['id'] for i in statuses]) - 1
+
+	# looper
+	while len(statuses) > 0:
+		statuses = cxn.statuses.user_timeline(max_id=oldest, **args)
+		if statuses:
+			df = build_dataframe(statuses, tz)
+
+			# append data to csv file
+			df.to_csv(path, index=False, encoding='utf-8', mode='a', \
+				header=False)
+			
+			oldest = min([i['id'] for i in statuses]) - 1
+
+	print (f'Data is available in {path}')
+	return path
+
+
 def timeline_one_round(cxn, **kwargs):
 	'''
 	'''
@@ -128,8 +347,7 @@ def timeline_one_round(cxn, **kwargs):
 def batch_timeline_one_round(cxn, **kwargs):
 	'''
 	'''
-	args = arguments(**kwargs)
-	if type(args['screen_name']) == str:
+	if type(kwargs['screen_name']) == str:
 		Error = 'screen_name should be a list, not string \
 			-> consider to use `timeline_one_round` instead'
 		Error = ' '.join(Error.split()).strip()
@@ -143,6 +361,14 @@ def batch_timeline_one_round(cxn, **kwargs):
 	pd.DataFrame(columns=twitter_timeline_columns()) \
 		.to_csv(path, index=False, encoding='utf-8')
 
+	# arguments
+	tz = 'default'
+	if 'tz' in kwargs.keys():
+		tz = kwargs['tz']
+		del kwargs['tz']
+	
+	args = arguments(**kwargs)
+
 	# iterate
 	for user in args['screen_name']:
 		tmp = {}
@@ -155,59 +381,13 @@ def batch_timeline_one_round(cxn, **kwargs):
 		# extract statuses
 		statuses = cxn.statuses.user_timeline(**tmp)
 
-
-
-
-
-		# temp
-		_id = [i['id_str'] for i in statuses]
-		tweet_url = [
-			f'https://twitter.com/i/web/status/{i}'
-			for i in _id
-		]
-		user = [i['user']['screen_name'] for i in statuses]
-		user_id_str = [i['user']['id_str'] for i in statuses]
-		user_url = [
-			f'https://twitter.com/{i}' for i in user
-		]
-		created_at = [i['created_at'] for i in statuses]
-		text = [i['full_text'] for i in statuses]
-		full_text = extract_full_text(statuses)
-		isrt = is_retweet_status(statuses)
-		isq = is_quote_status(statuses)
-		source = [get_anchor_text(i['source']) for i in statuses]
-
-		# retweet attributes
-		rt_attrs = retweet_attributes(statuses)
-
-		df = pd.DataFrame({
-			'id_str': _id,
-			'tweet_url': tweet_url,
-			'user': user,
-			'user_id_str': user_id_str,
-			'user_url': user_url,
-			'created_at': created_at,
-			'text': text,
-			'full_text': full_text,
-			'is_retweet_status': isrt,
-			'rt_user_screen_name': rt_attrs['rt_user_screen_name'],
-			'rt_user_id_str': rt_attrs['rt_user_id_str'],
-			'rt_status_id_str': rt_attrs['rt_status_id_str'],
-			'is_quote_status': isq,
-			'source': source
-		})
-
-		# clean text
-		df['text'] = df['text'].apply(clean_text)
-		df['full_text'] = df['full_text'].apply(clean_text)
-
-
-
-
-
+		# build dataframe
+		df = build_dataframe(statuses, tz)
 
 		# append data to csv file
 		df.to_csv(path, index=False, encoding='utf-8', mode='a', \
 			header=False)
 
-	return folder
+
+	print (f'Data is available in {path}')
+	return path
